d75da8e52f4080a6344d6d7f4e92fbc790644bbf
Victor <vlenceleuth@gmail.com>
2025-08-22 06:55:42 +0530

    Setting with any value

--- setting.go
+++ setting.go
@@ -5,21 +5,9 @@ import (
         "github.com/vlence/gossert"
 )
 
-// Represents a name-value pair.
-type NamedValue interface {
-        // Name returns the name of the setting.
-        Name() string
-
-        // Type returns the type of the setting's value.
-        Type() Type
-
-        // String returns the setting's value as a string.
-        String() string
-}
-
 // A name-value pair. T must conform to one of the Type values exported,
 // except Unsupported.
-type Setting[T any] struct {
+type Setting struct {
         hasName
         hasDescription
         canBeDeprecated
@@ -27,17 +15,22 @@ type Setting[T any] struct {
         canBeUpdated
 
         typ Type
-        value T
+        value any
 }
 
-func (setting *Setting[T]) Type() Type {
+func (setting *Setting) Type() Type {
         gossert.Ok(nil != setting, "setting: cannot return type of nil setting")
         return setting.typ
 }
 
-func (setting *Setting[T]) String() string {
-        gossert.Ok(nil != setting, "setting: cannot return string representation of nil setting")
+func (setting *Setting) String() string {
+        gossert.Ok(nil != setting, "setting: cannot return value of nil setting as string")
 
         // todo
         return ""
+}
+
+func (setting *Setting) Value() any {
+        gossert.Ok(nil != setting, "setting: cannot return value of nil setting")
+        return setting.value
 }
